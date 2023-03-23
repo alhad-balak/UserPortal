d2b472c5a60543ca97fc466cd79171b5e7e29365
abhin <abhinav.iit19@gmail.com>
2023-03-23 16:28:01 +0530

    Add project files.

---
+++ SQLQuery1.sql
@@ -0,0 +1,3 @@
+﻿INSERT INTO userlist (username, name, email, phone, address, manager_name)
+VALUES
+('abhinav', 'Test1', 'test1.check@gmail.com', '+911234567890', 'Varanasi, UP, India', 'Mr. Dubey');
